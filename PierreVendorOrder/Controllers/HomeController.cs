b39fb0a4db6166a73ae4f59955526d1a5e99050c
Ashe Urban <ashe@goldentongue.com>
2022-07-22 11:57:00 -0700

    ADD order functionality and routes complete.

--- PierreVendorOrder/Controllers/HomeController.cs
+++ PierreVendorOrder/Controllers/HomeController.cs
@@ -1,15 +1,28 @@
-// using Microsoft.AspNetCore.Mvc;
-// using PierreVendorOrder.Models;
+using Microsoft.AspNetCore.Mvc;
+using PierreVendorOrder.Models;
 
-// namespace PierreVendorOrder.Controllers
-// {
-//   public class HomeController : Controller
-//   {
-//     [Route("/")]
-//   public ActionResult Index()
-//   {
-//     Order starterOrder = new Order("Add a New Order");
-//     return View(starterOrder);
-//   }
-//   }
-// }
+namespace PierreVendorOrder.Controllers
+{
+  public class HomeController : Controller
+  {
+  [Route("/")]
+  public ActionResult Index()
+  {
+    Order starterOrder = new Order("Add a New Order");
+    return View(starterOrder);
+  }
+
+  [Route("/orders/new")]
+  public ActionResult CreateForm()
+  {
+    return View();
+  }
+
+  [Route("orders")]
+  public ActionResult Create(string description)
+  {
+    Order myOrder = new Order(description);
+    return View("Index", myOrder);
+  }
+  }
+}
