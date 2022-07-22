df93e6f94241f5bd0d2c471fff23fd9ea9ecb77a
Ashe Urban <ashe@goldentongue.com>
2022-07-22 13:13:31 -0700

    ADD, FAIL, PASS third Vendor test.

--- PierreVendorOrder.Tests/ModelTests/VendorTests.cs
+++ PierreVendorOrder.Tests/ModelTests/VendorTests.cs
@@ -42,6 +42,18 @@ namespace PierreVendorOrder.Tests
       CollectionAssert.AreEqual(newList, result);
     }
 
+    [TestMethod]
+    public void Find_ReturnsCorrectVendor_Vendor()
+    {
+
+      string name01 = "New Seasons";
+      string name02 = "Extracto";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+      Vendor result = Vendor.Find(2);
+      Assert.AreEqual(newVendor2, result);
+    }
+
 
 
 
--- PierreVendorOrder/Models/Vendor.cs
+++ PierreVendorOrder/Models/Vendor.cs
@@ -27,6 +27,11 @@ namespace PierreVendorOrder.Models
       return _instances;
     }
 
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
 
   }
 }
