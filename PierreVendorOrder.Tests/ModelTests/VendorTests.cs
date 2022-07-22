b34e363d7c715d76c27024436e4fe078286f16cf
Ashe Urban <ashe@goldentongue.com>
2022-07-22 13:03:57 -0700

    ADD, FAIL, PASS first Vendor tests for creating Vendor obj and assigning Id.

---
+++ PierreVendorOrder.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierreVendorOrder.Models;
+using System.Collections.Generic;
+using System;
+
+namespace PierreVendorOrder.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+      int result = newVendor.Id;
+      Assert.AreEqual(1, result);
+    }
+
+
+
+  }
+}
